e1b025c78da7adbc491b3c13b287f9bbc74a8ada
orel aharonov <orel@invarial.com>
2022-07-07 12:17:44 +0300

    added data to action

--- LearnC/preprocessor/Preprocessor/ActionInfo.h
+++ LearnC/preprocessor/Preprocessor/ActionInfo.h
@@ -1,10 +1,11 @@
 #ifndef ACTION_INFO_H
 #define ACTION_INFO_H
 
-#include "Command.h"
+#include "PreprocessData.h"
 
 typedef struct ActionInfo
 {
+	PreprocessData data;
 	char** argv;
 	int argc;
 } ActionInfo;
