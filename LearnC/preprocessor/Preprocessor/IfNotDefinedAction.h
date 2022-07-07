2a3ca3c00ea288143fdc990bf7896c334f406df3
orel aharonov <orel@invarial.com>
2022-07-07 18:35:09 +0300

    implemented ifndef - preprocessor

--- LearnC/preprocessor/Preprocessor/IfNotDefinedAction.h
+++ LearnC/preprocessor/Preprocessor/IfNotDefinedAction.h
@@ -1,13 +1,62 @@
 #ifndef IFNDEF_H
 #define IFNDEF_H
 
-#include "Command.h"
 #include "ActionInfo.h"
 #include "ReturnCode.h"
+#include "Controller.h"
+
+#define ELSE "else"
+#define ENDIF "endif"
 
 ReturnCode IfNotDefined(ActionInfo info)
 {
-	return SUCCESS;
+	ReturnCode result = SUCCESS;
+	Definition* definition = NULL;
+	char* command = NULL;
+	
+	if (info.argc == 1)
+	{
+		definition = searchDefinitionByName(&info.controller->data, info.argv[0]);
+		
+		if (definition == NULL)
+		{
+			do
+			{
+				if (command != NULL)
+				{
+					free(command);
+				}
+				command = getNextCommand(info.controller);
+				result = runCommand(info.controller, command);
+
+			} while (strcmp(command, ELSE) != 0 || strcmp(command, ENDIF) != 0);
+
+			while (strcmp(command, ENDIF) != 0)
+			{
+				if (command != NULL)
+				{
+					free(command);
+				}
+				command = getNextCommand(info.controller);
+			}
+			free(command);
+		}
+		else
+		{
+			do
+			{
+				if (command != NULL)
+				{
+					free(command);
+				}
+				command = getNextCommand(info.controller);
+			} while (strcmp(command, ELSE) != 0 || strcmp(command, ENDIF) != 0);
+			
+			free(command);
+		}
+	}
+
+	return result;
 }
 
 #endif
